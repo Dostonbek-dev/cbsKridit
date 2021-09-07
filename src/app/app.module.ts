6cf484d3de41c89d7fbda97b3ffd62ba0adf3508
dostonbekkhamidov@gmail.com <doston1994iyul23>
2021-09-07 09:40:37 +0500

    fix gitHub bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,39 +12,68 @@ import {SplitButtonModule} from "primeng/splitbutton";
 import {ToolbarModule} from "primeng/toolbar";
 import {ConfirmDialogModule} from "primeng/confirmdialog";
 import {ToastModule} from "primeng/toast";
-import { HeaderComponent } from './header/header.component';
+import {HeaderComponent} from './header/header.component';
 import {RouterModule, Routes} from "@angular/router";
 import {RippleModule} from "primeng/ripple";
 import {LoanRequestComponent} from "./modules/loan-request/loan-request/loan-request.component";
 import {FieldsetModule} from "primeng/fieldset";
-import { LoginComponent } from './auth/login/login.component';
-import {TableModule} from "primeng/table";
-import {DialogModule} from "primeng/dialog";
-import {PaginatorModule} from "primeng/paginator";
-import {RadioButtonModule} from "primeng/radiobutton";
+import {LoginComponent} from './auth/login/login.component';
+import {NikiComponent} from './modules/niki/niki/niki.component';
+import {KatmComponent} from './modules/katm/katm/katm.component';
+import {CRegistryComponent} from './modules/c-registry/c-registry/c-registry.component';
 import {InputNumberModule} from "primeng/inputnumber";
+import {FormsModule} from "@angular/forms";
+import {RadioButtonModule} from "primeng/radiobutton";
+import {DropdownModule} from "primeng/dropdown";
+import {TableModule} from "primeng/table";
+import {FileUploadModule} from "primeng/fileupload";
+import {DialogModule} from "primeng/dialog";
 import {RatingModule} from "primeng/rating";
+import { HttpClientModule } from '@angular/common/http';
+import {ProductService} from "./product.service";
+import {InputTextareaModule} from "primeng/inputtextarea";
 
-const  routes:Routes=[
+const routes: Routes = [
   {
     path: '',
     component: LoginComponent
-   },
+  },
   {
     path: 'loanRequest',
     component: LoanRequestComponent
   }
+  ,
+  {
+    path: 'niki',
+    component: NikiComponent
+  }
+  ,
+  {
+    path: 'katm',
+    component: KatmComponent
+  }
+  ,
+  {
+    path: 'c-registry',
+    component: CRegistryComponent
+  },
+  { path: '**', redirectTo: 'error' }
 ]
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoanRequestComponent,
-    LoginComponent
+    LoginComponent,
+    NikiComponent,
+    KatmComponent,
+    CRegistryComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
+    HttpClientModule,
     MenubarModule,
     BrowserAnimationsModule,
     SharedModule,
@@ -58,11 +87,14 @@ const  routes:Routes=[
     RippleModule,
     FieldsetModule,
     TableModule,
+    RatingModule,
     DialogModule,
-    PaginatorModule,
+    FormsModule,
+    DropdownModule,
     RadioButtonModule,
     InputNumberModule,
-    RatingModule
+    FileUploadModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]

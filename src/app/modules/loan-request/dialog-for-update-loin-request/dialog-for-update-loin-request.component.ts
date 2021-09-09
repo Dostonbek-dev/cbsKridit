143b12ea886bccae4e1c2b5412c9957bf06b2dfb
dostonbekkhamidov@gmail.com <doston1994iyul23>
2021-09-09 19:57:12 +0500

    dialog for update request

---
+++ src/app/modules/loan-request/dialog-for-update-loin-request/dialog-for-update-loin-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-for-update-loin-request',
+  templateUrl: './dialog-for-update-loin-request.component.html',
+  styleUrls: ['./dialog-for-update-loin-request.component.css']
+})
+export class DialogForUpdateLoinRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
